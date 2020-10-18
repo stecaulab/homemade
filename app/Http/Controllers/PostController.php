8f5e9f00ec396babea133ddc7a9290298fe30578
stecaulab <cauzzistefano@icloud.com>
2020-10-18 17:18:24 +0100

    add all() and single() method for view

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $post   =   new Post();
+        $post->user_id  =   2;
+        $post->title= "Using laravel Seeders";
+        $post->body =lorem*50
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+
+
+    /**
+     * He retrieve five created posts per page and send to landing view.
+     */
+     public function all()
+    {
+        return view('landing',[
+
+                'posts' =>  Post::latest()->paginate(5)
+        ]);
+    }
+
+    /**
+     * Return the view for the single post
+     * Usiamo una funzionalità di laravel 'model binding' per mappare ilparametro URL
+     * a un istanza Post con lo stesso ID
+     */
+
+    public function single(Post $post){
+
+        return view('single', compact('post'));
+    }
+}
