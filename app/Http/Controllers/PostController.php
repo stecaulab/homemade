c2c03ea125b1a1fbc655afdaa60cfd10a1fdd4ca
stecaulab <cauzzistefano@icloud.com>
2020-10-22 20:22:37 +0100

    modifed store() method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Resources\PostResource;
 
 class PostController extends Controller
 {
@@ -35,7 +37,35 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+
+                'title'         =>      'required',
+                'body'          =>      'required',
+                'user_id'       =>      'required',
+                'image'         =>      'required|mimes:jpeg,png,jpg,gif,svg',
+        ]);
+
+        $post   = new Post;
+
+
+        if($request->hasFile('image')) {
+
+            $image              =   $request->file('image');
+            $name               =   Str::slug($request->title).'.'.$image->getClientOriginalExtension();
+            $destinationPath    =   public_path('/uploads/posts');
+            $imagePath          =   $destinationPath . "/" . $name;
+            $image->move($destinationPath, $name);
+            $post->image        = $name;
+        }
+
+        $post->user_id          =   $request->user_id;
+        $post->title            =   $request->title;
+        $post->body             =   $request->body;
+
+        $post->save();
+
+        return new PostResource($post);
+
     }
 
     /**
