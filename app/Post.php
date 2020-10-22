75a908961681102407ce685f7c086499abb1ab00
stecaulab <cauzzistefano@icloud.com>
2020-10-22 19:51:25 +0100

    add filled field

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    //field if you can to be assigned data
+
+    protected $fillable =   ['user_id','title','body','image'];
+
     public function user(){
 
         return $this->belongsTo(User::class);
