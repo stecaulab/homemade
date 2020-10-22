7e42a3438af384deaa7a1c712f08ce05c76a5685
stecaulab <cauzzistefano@icloud.com>
2020-10-22 19:52:01 +0100

    add header section

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,6 +13,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- fonts -->
+
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
     <!-- Styles -->
     <link rel="stylesheet" href="{!! asset('css/app.css') !!}">
     <link rel="stylesheet" href="{!! asset('css/clean-blog.css') !!}">
@@ -85,7 +90,19 @@
             </div>
         </div>
     </nav>
-    
+    <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <div class="site-heading">
+                <h1>Clean Blog</h1>
+                <span class="subheading">A Blog Theme by Start Bootstrap</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
 
      <!-- Content -->
 
