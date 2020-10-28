baba56994669b0adad3332c39df995cd1b8f3de3
stecaulab <cauzzistefano@icloud.com>
2020-10-28 22:04:35 +0100

    modified url redirect

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
     {
         if($request->user()->hasRole('user')){
 
-            return redirect('home');
+            return redirect('/');
         }
 
         if($request->user()->hasRole('admin')){
